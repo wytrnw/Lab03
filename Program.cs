15781d96acd8fc41c86f5012df53c07b1f4642ac
Плохова Лера <lerkaaa28@mail.ru>
2026-09-23 22:18:53 +0300

    Lab03: compound assignment operators

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Банковский счёт");
+
+double balance = 1000;
+Console.WriteLine($"Начальный баланс: {balance}");
+
+balance += 500;
+Console.WriteLine($"После пополнения на 500: {balance}");
+
+balance -= 200;
+Console.WriteLine($"После покупки на 200: {balance}");
+
+balance *= 1.05;
+Console.WriteLine($"После начисления 5%: {balance}");
+
+balance /= 2;
+Console.WriteLine($"После деления пополам: {balance}");
